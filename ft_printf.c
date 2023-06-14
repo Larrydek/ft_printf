4e52716f509a357d60e776895c4820fcc5aeb57c
Juan Manuel De Cleene <jde-clee@c1r9s1.42madrid.com>
2023-06-14 20:01:43 +0200

    va_args | main created

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jde-clee <jde-clee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 19:09:44 by jde-clee          #+#    #+#             */
-/*   Updated: 2023/06/14 19:25:18 by jde-clee         ###   ########.fr       */
+/*   Updated: 2023/06/14 20:00:16 by jde-clee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,61 @@ int	ft_printf(char const *type, ...)
 {
     va_list args;
     va_start(args, type);
+    int i;
 
-    if(type[0] == '%')
+    i = 0;
+    while(*type)
     {
-        if (type[1] == 'c')
-            write(1, args, 1)
+        if(*type == '%')
+        {
+            type++;
+            if (*type == 'c')
+                write(1, args[i], 1)
+        }
+        else if(*type == 's')
+        {
+            
+        }
+        else if(*type == 'p')
+        {
+            
+        }
+        else if(*type == 'd')
+        {
+            
+        }
+        else if(*type == 'u')
+        {
+            
+        }
+        else if(*type == 'x')
+        {
+            
+        }
+        else if(*type == 'X')
+        {
+            
+        }
+        else if(*type == '%')
+        {
+            
+        }
+        type++;
     }
-
+    va_end(args);
     
     return (1);
+}
+
+int main()
+{
+    int entero = 42;
+    unsigned int entero_sin_signo = 12345;
+    unsigned int hexadecimal = 0xABCD;
+    char caracter = 'A';
+    char* cadena = "Hola, mundo!";
+    void* puntero = &entero;
+
+    ft_printf("Entero: %d, Sin signo: %u, Hexadecimal: %x, Caracter: %c, Cadena: %s, Puntero: %p\n", entero, entero_sin_signo, hexadecimal, caracter, cadena, puntero);
+    return (0);
 }
