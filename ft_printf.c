8676b30638a2a1a801ef0775202ae908a7cddeeb
Juan Manuel De Cleene <jde-clee@c1r3s2.42madrid.com>
2023-09-06 20:58:01 +0200

    printf comeback

--- ft_printf.c
+++ ft_printf.c
@@ -6,65 +6,62 @@
 /*   By: jde-clee <jde-clee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 19:09:44 by jde-clee          #+#    #+#             */
-/*   Updated: 2023/09/05 16:49:21 by jde-clee         ###   ########.fr       */
+/*   Updated: 2023/09/06 20:55:32 by jde-clee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
+int ft_format(char const *s, char type, int len)
+{
+    if (*type == 'c')
+    {
+    }
+    else if (*type == 's')
+    {
+    }
+    else if (*type == 'p')
+    {
+    }
+    else if (*type == 'd')
+    {
+    }
+    else if (*type == 'u')
+    {
+    }
+    else if (*type == 'x')
+    {
+    }
+    else if (*type == 'X')
+    {
+    }
+    else if (*type == '%')
+    {  
+    }
+}
 
-
-int	ft_printf(char const *type, ...)
+int ft_printf(char const *type, ...)
 {
     va_list args;
+    
     va_start(args, type);
-    int i;
+    int len;
 
-    i = 0;
+    len = 0;
     while(*type)
     {
         if(*type == '%')
         {
             type++;
-            if (*type == 'c')
-                write(1, args[i], 1);
-            else if (*type == 's')
-            {
-                write(1, args[i], ft_strlen(args[i]));
-            }
-            else if (*type == 'p')
-            {
-                write(1, &args[i], ft_strlen(args[i]))
-            }
-            else if (*type == 'd')
-            {
-                ft_putnbr_fd(args[i], 1);
-            }
-            else if (*type == 'u')
-            {
-                if (args[i] < 0)
-                    args[i] = args[i] * -1;
-                write(1, args[i],)
-            }
-            else if (*type == 'x')
-            {
-                
-            }
-            else if (*type == 'X')
-            {
-                
-            }
-            else if (*type == '%')
-            {
-                
-            }
+            ft_format(type, args, len);
+            
         }
         
         type++;
     }
     va_end(args);
     
-    return (1);
+    return (len);
 }
 
 int main()
